a162ea3fd628f1363f8d55acbe2e5e6a1eb0c03b
Ayomide-Daniel <ayomidedaniel00@gmail.com>
2022-01-13 10:20:43 +0100

    feat: pdo fetch_obj

--- question-1.php
+++ question-1.php
@@ -8,25 +8,19 @@ class Connection{
     protected function conn(){
         $dsn = 'mysql:host='.$this->host.';dbname='.$this->db;
         $pdo = new PDO($dsn, $this->user, $this->pwd);
-        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
         return $pdo;
     }
 }
-class User extends Connection{
-    private $id, $username, $full_name, $profile_picture, $followed;
-     
+class User extends Connection{     
     public function getUser($user_id, $auth_user_id)
     {
         $sql = "SELECT * FROM users WHERE id = ?";  
         $stmt = $this->conn()->prepare($sql);
         $stmt->execute([$user_id]);
         $user = $stmt->fetch();
-        $this->id = $user['id'];
-        $this->username = $user['username'];
-        $this->full_name = $user['full_name'];
-        $this->profile_picture = $user['profile_picture'];
-        $this->followed = $this->isFollowed($auth_user_id, $user_id);
-        return $this;
+        $user->followed = $this->isFollowed($auth_user_id, $user_id);
+        return $user;
     }
 
     public function isFollowed($auth_user_id, $user_id)
@@ -34,18 +28,16 @@ class User extends Connection{
         $sql =  "SELECT * FROM followers WHERE follower_id = ? AND following_id =  ?";
         $stmt = $this->conn()->prepare($sql);
         $stmt->execute([$auth_user_id, $user_id]);
-        $result = $stmt->fetchAll();
+        $result = $stmt->fetch();
         if(count($result) > 0){
-            return true;
+            return "true";
         }else{
-            return false;
+            return "false";
         }
     }
 }
 
 class Post extends Connection{
-    private $id, $description, $owner, $image, $created_at, $liked;
-
     public function getPost($user_id, $posts_ids)
     {
         $posts = array();
@@ -53,14 +45,10 @@ class Post extends Connection{
         $stmt = $this->conn()->query($sql);
         $result = $stmt->fetchAll();
         foreach ($result as $key => $post) {
-            $this->id = $post['id'];
-            $this->description = $post['description'];
             $owner = new User();
-            $this->owner = $owner->getUser($post['user_id'], $user_id);
-            $this->image = $post['image'];
-            $this->created_at = $post['created_at'];
-            $this->liked = $this->isLiked($user_id, $post['id']);
-            array_push($posts, $this);
+            $post->owner = $owner->getUser($post->user_id, $user_id);
+            $post->liked = $this->isLiked($user_id, $post->id);
+            array_push($posts, $post);
         }
         return $posts;
     }
@@ -70,11 +58,11 @@ class Post extends Connection{
         $sql =  "SELECT * FROM likes WHERE user_id = ? AND post_id = ?";
         $stmt = $this->conn()->prepare($sql);
         $stmt->execute([$user_id, $post_id]);
-        $result = $stmt->fetchAll();
+        $result = $stmt->fetch();
         if(count($result) > 0){
-            return true;
+            return "true";
         }else{
-            return false;
+            return "false";
         }
     }
 }
